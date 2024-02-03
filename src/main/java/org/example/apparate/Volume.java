392a3f05defd5f84652be409d9123deb579098fe
bulat <bulat.sabitov.82@mail.ru>
2024-02-03 13:52:33 +0300

    set change Volume

--- src/main/java/org/example/apparate/Volume.java
+++ src/main/java/org/example/apparate/Volume.java
@@ -1,4 +1,26 @@
 package org.example.apparate;
 
+import java.io.IOException;
+
 public class Volume {
+    //Brightness change
+    private static final int increaseBy = 10;
+    private final static String increaseVolume = "osascript -e \"set volume output volume (output volume of (get volume settings) + " + increaseBy + ")\"";
+    private final static String decreaseVolume = "osascript -e \"set volume output volume (output volume of (get volume settings) + " + -increaseBy + ")\"";
+    static Runtime runtime = Runtime.getRuntime();
+    public static void increase(){
+        try {
+            Process process  = runtime.exec(increaseVolume);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void decrease(){
+        try {
+            Process process  = runtime.exec(decreaseVolume);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
